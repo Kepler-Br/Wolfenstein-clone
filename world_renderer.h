d8554829a6243bc1ed0b2b5e5755f76aa4a1caa9
Barrett Kepler <barrett.kepler@gmail.com>
2020-06-11 07:41:52 +0300

    Rudimental WIP multithreading rendering

--- world_renderer.h
+++ world_renderer.h
@@ -9,7 +9,10 @@
 #include "world.h"
 #include "player.h"
 #include "sdl_wrapper.h"
-#include <SDL2/SDL_image.h>
+#include "world_renderer_thread.h"
+#include <thread>
+#include <mutex>
+#include <queue>
 
 class World_renderer {
 private:
@@ -19,7 +22,13 @@ private:
     Sdl_wrapper &sdl_wrapper;
     float field_of_view = 70.0f*M_PI/180.0f;
     int blockiness = 1;
-
+//    constexpr static int threads_total = 4;
+//    std::thread *threads;
+//    World_renderer_thread *renderers;
+//    std::mutex render_mutex;
+//    std::mutex task_wait_mutex;
+//    std::queue<std::pair<glm::vec2, glm::ivec2>> tasks;
+//    std::condition_variable new_tasks;
     void render_view_rays(const glm::vec2 &center, const float &size);
     void render_one_block_view_rays(const glm::vec2 &center, const float &size);
     void render_blocks(const glm::vec2 &center, const float &size, const bool fill_screen);
@@ -27,6 +36,7 @@ private:
 
 public:
     World_renderer(World &world, const Raycaster &raycaster, const Player &player, Sdl_wrapper &sdl_wrapper);
+    ~World_renderer();
     void render();
     void render_map(const bool render_rays, const bool fill_screen, const float size = 1.0f);
 
